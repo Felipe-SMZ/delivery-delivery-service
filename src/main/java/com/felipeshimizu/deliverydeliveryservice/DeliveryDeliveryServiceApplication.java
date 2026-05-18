3ba859ef810d8697ccfa378cfe096db1e199e18a
Felipe <felipeshimizu5@gmail.com>
2026-05-18 20:37:34 -0300

    first commit

---
+++ src/main/java/com/felipeshimizu/deliverydeliveryservice/DeliveryDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.felipeshimizu.deliverydeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryDeliveryServiceApplication.class, args);
+    }
+
+}
