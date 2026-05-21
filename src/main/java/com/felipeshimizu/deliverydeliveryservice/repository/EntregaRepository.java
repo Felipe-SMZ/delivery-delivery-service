0f22d28c97521cf0ed8ad1c4347fe5500e35c625
Felipe <felipeshimizu5@gmail.com>
2026-05-21 00:19:48 -0300

    feat: configurando a classe principal

--- src/main/java/com/felipeshimizu/deliverydeliveryservice/repository/EntregaRepository.java
+++ src/main/java/com/felipeshimizu/deliverydeliveryservice/repository/EntregaRepository.java
@@ -3,8 +3,11 @@ package com.felipeshimizu.deliverydeliveryservice.repository;
 import com.felipeshimizu.deliverydeliveryservice.model.Entrega;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EntregaRepository extends JpaRepository<Entrega, UUID> {
     boolean existsByPedidoId(UUID pedidoId);
+
+    Optional<Entrega> findByPedidoId(UUID pedidoId);
 }
