5e5826681e053a1ca3d9717207eee364b7585dc9
Felipe <felipeshimizu5@gmail.com>
2026-05-21 00:19:32 -0300

    feat: criando o erro notfoundexception

---
+++ src/main/java/com/felipeshimizu/deliverydeliveryservice/exception/EntregaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.felipeshimizu.deliverydeliveryservice.exception;
+
+public class EntregaNotFoundException extends RuntimeException {
+    public EntregaNotFoundException(String message) {
+        super(message);
+    }
+}
